2815b873741987a2093ac94396e2dacaa1c3cb35
Michael Tamirie <michaeltamirie@gmail.com>
2022-04-28 13:40:20 +0300

    Fix the github workflow case

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -11,9 +11,9 @@ from extract_dataframe import TweetDfExtractor
 from extract_dataframe import read_json
 
 # Unzipping the data and reading it
-with zipfile.ZipFile("../data/Economic_Twitter_Data.zip", "r") as unzipping:
+with zipfile.ZipFile("data/Economic_Twitter_Data.zip", "r") as unzipping:
     unzipping.extractall("data/")
-_, tweet_list = read_json("../data/Economic_Twitter_Data.json")
+_, tweet_list = read_json("data/Economic_Twitter_Data.json")
 
 columns = ['created_at', 'source', 'original_text', 'clean_text', 'sentiment', 'polarity', 'subjectivity', 'lang', 'favorite_count', 'retweet_count',
            'original_author', 'screen_count', 'followers_count', 'friends_count', 'possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
